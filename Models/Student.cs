ec5e718fe80f9353e81629e7968f277df56f2288
kellycoles <kellycoles1@gmail.com>
2019-10-30 10:09:02 -0500

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExecisesAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SlackHandle { get; set; }
+
+        //T his is to hold the actual foreign key integer
+        public int CohortId { get; set; }
+
+        // This property is for storing the C# object representing the department
+        public Cohort Cohort { get; set; }
+    }
+}
